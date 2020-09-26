5d31e99a8ea6afe8a2b39c86afa1dd81430a6642
dboppana <dboppana@host.docker.internal>
2020-09-26 05:51:12 -0700

    maven successfull

---
+++ mavenjenk/src/main/java/com/dxc/mavenjenk/Hello.java
@@ -0,0 +1,11 @@
+package com.dxc.mavenjenk;
+
+public class Hello {
+public static void main(String[] args) {
+		
+		System.out.println("Hello Jenkins");
+		
+		System.out.println("Run in Maven");
+		
+	}
+}
